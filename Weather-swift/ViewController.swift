36664ae3adef2ac1af534c59bb82a0d2d767c7c7
Kirill Pyulzyu <kirill.pyulzyu@gmail.com>
2017-04-13 00:53:00 +0300

    Initial commit

---
+++ Weather-swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Weather-swift
+//
+//  Created by Kirill Pyulzyu on 13.04.17.
+//  Copyright (c) 2017 Kirill Pyulzyu. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+
+
+
+}
