9261404575d091f3976aa5d96026971e696c40ae
Kirill Pyulzyu <kirill.pyulzyu@gmail.com>
2017-04-13 01:37:49 +0300

    - get data from server

--- Weather-swift/ViewController.swift
+++ Weather-swift/ViewController.swift
@@ -14,12 +14,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+        self.getData()
     }
 
 
     override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+    }
+
+
+    func getData() {
+        let defaultSession = URLSession(configuration: URLSessionConfiguration.default)
+        var dataTask: URLSessionDataTask?
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=c4d1e7389a2e0605f6e44bd8d42c97e7")
+
+
+        dataTask = defaultSession.dataTask(with: url!){
+            data, response, error in
+
+            DispatchQueue.main.async() {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    let dict = try! JSONSerialization.jsonObject(with: data!)
+                    print(dict);
+                }
+            }
+        }
+
+        dataTask?.resume()
     }
 
 
